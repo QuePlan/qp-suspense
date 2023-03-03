9511c8085940cc8c79931ef285a9a75ade949ec4
Jose Ignacio Santa Cruz <jsanta@queplan.cl>
2023-03-03 15:11:51 -0300

    feat: :sparkles: ADD agrega componente de test en modo supsenseable (ejemplo para ver  imports automaticos desde la clase abstracta)

---
+++ src/app/test-suspenseable/test-suspenseable.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Suspenseable } from '../types';
+
+@Component({
+  selector: 'app-test-suspenseable',
+  templateUrl: './test-suspenseable.component.html',
+  styleUrls: ['./test-suspenseable.component.scss']
+})
+export class TestSuspenseableComponent extends Suspenseable {
+  ngOnInit(): void {}
+  ngOnDestroy(): void {}
+
+  eventHandler(eventName: string): void {
+    this.eventName = eventName;
+  }
+
+}
