e9b64271e204034e66263311e8fdaeb5b8d4ddbd
SebaVRdev <sebarm177@gmail.com>
2023-03-08 17:15:07 -1100

    feat(refactor) DEL Se elimino archivos que se pasaron a la libreria

--- src/app/error-view.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, TemplateRef } from '@angular/core';
-
-@Directive({
-  selector: '[errorView]',
-})
-export class ErrorViewDirective {
-  constructor(public tpl: TemplateRef<any>) {}
-}
--- src/app/fallback-view.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, TemplateRef } from '@angular/core';
-
-@Directive({
-  selector: '[fallbackView]',
-})
-export class FallbackViewDirective {
-  constructor(public tpl: TemplateRef<any>) {}
-}
