445ae8d7fc8ba1521e699076eb4cc9f0f694bd15
Jose Ignacio Santa Cruz G <j@santa.cl>
2023-03-13 17:57:09 -0300

    docs(qp-suspense): :memo: Documentacion para directivas

--- libs/suspense/src/lib/directives/error-view.directive.ts
+++ libs/suspense/src/lib/directives/error-view.directive.ts
@@ -1,5 +1,10 @@
 import { Directive, TemplateRef } from '@angular/core';
 
+/**
+ * Directiva para definir el área contenedora donde se desplegará el
+ * componente/contenido asociado al estado de error de carga de un 
+ * componente de tipo Suspenseable.
+ */
 @Directive({
   selector: '[errorView]',
   standalone: true,
