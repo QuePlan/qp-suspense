82c078d7d7d8ee4813aef0b353b100a12fd1f9b7
Jose Ignacio Santa Cruz G <j@santa.cl>
2023-03-13 14:03:39 -0300

    refactor(qp-suspense): :art: CHG elimina import no utiliazdo

--- apps/qp-suspense-demo/src/app/lazy-standalone/lazy-standalone.component.ts
+++ apps/qp-suspense-demo/src/app/lazy-standalone/lazy-standalone.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Suspenseable, SuspenseableBroadcaster } from '@queplan/suspense';
+import { SuspenseableBroadcaster } from '@queplan/suspense';
 import { tap, timer } from 'rxjs';
 
 @Component({
