1cfeabfc5a2720500242288936c57468e10a182a
Jose Ignacio Santa Cruz G <j@santa.cl>
2023-03-14 09:23:45 -0300

    docs(qp-suspense): :memo: CHG Agrega documentacion de uso detallada

--- libs/suspense/src/lib/types/suspenseable-broadcaster.ts
+++ libs/suspense/src/lib/types/suspenseable-broadcaster.ts
@@ -9,20 +9,25 @@ import { ISuspenseable, SuspenseableRenderer } from "./types";
  * Sólo fuerza la definición del método `eventHandler()`, que debe definir el nombre del evento
  * a capturar.
  * 
- * Modo reactivo (el componente se despliega una vez recibido el evento `<eventName>:load`):
- * ```
- * <qp-suspense>
- *  <ng-template [queplanDefaultView]="compFactory" [componentParams]="compParams" clazzName="CompComponent" onEvent="comp"></ng-template>
+ * Modo reactivo 
+ * ----------------
  * 
- *  <ng-template queplanFallbackView><qp-suspense-spinner>Cargando Comp...</qp-suspense-spinner></ng-template>
- *  <ng-template queplanErrorView><qp-suspense-error>Falló carga de Comp</qp-suspense-error></ng-template>
- * </qp-suspense>
+ * El componente se despliega una vez recibido el evento `<eventName>:load`:
+ * 
  * ```
+ * <suspense>
+ *  <ng-template [defaultView]="compFactory" [componentParams]="compParams" clazzName="CompComponent" onEvent="comp"></ng-template>
+ * 
+ *  <ng-template fallbackView><qp-suspense-spinner>Cargando Comp...</qp-suspense-spinner></ng-template>
+ *  <ng-template errorView><qp-suspense-error>Falló carga de Comp</qp-suspense-error></ng-template>
+ * </suspense>
+ * ```
+ * 
  * El nombre del evento debe ser el mismo que el nombre del componente SuspenseableBroadcaster, sin el sufijo "Component";
- * y debe ser indicado como el @Input() `onEvent` del componente.
+ * y debe ser indicado como el @Input() `onEvent` del componente.  
  * Si se especifica onEvent con algún error, el componente no se desplegará y el mensaje de error indicará la corrección a realizar.
  * En este modo, una vez que el componente realice las operaciones que lo disponibilizan para ser desplegado, se deberá incluir la 
- * correspondiente llamada al método `this.broadcastLoad()`.
+ * correspondiente llamada al método `this.broadcastLoad()`.  
  * Análogamente, en caso de error se deberá llamar al método `this.broadcastError()`.
  */
 export abstract class SuspenseableBroadcaster extends SuspenseableRenderer implements Pick<ISuspenseable, 'eventHandler' | 'broadcastLoad' | 'broadcastError'> {
