f6c75558bf64679484de5e0a450571059e7d48cd
Jose Ignacio Santa Cruz G <j@santa.cl>
2023-03-20 18:43:48 -0300

    style(qp-suspense): :art: CHG indentacion

--- apps/qp-suspense-demo/src/app/not-lazy2/not-lazy2.component.ts
+++ apps/qp-suspense-demo/src/app/not-lazy2/not-lazy2.component.ts
@@ -7,7 +7,7 @@ import { SuspenseableClassic, useSuspense } from '@queplan/qp-suspense/types';
   selector: 'app-not-lazy2',
   templateUrl: './not-lazy2.component.html',
   styleUrls: ['./not-lazy2.component.scss'],
-  providers: [useSuspense(NotLazy2Component)],
+  providers: [ useSuspense(NotLazy2Component) ],
 })
 export class NotLazy2Component extends SuspenseableClassic {
   @Input() saludos: string;
