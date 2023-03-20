6ce0969b12a3ee7bfa078aafa7b464c9e4802cbd
Jose Ignacio Santa Cruz G <j@santa.cl>
2023-03-20 18:47:43 -0300

    feat(qp-suspense): :sparkles: CHG simplifica aplicacion  pasando todos los ejemplos a la pagina de home

--- apps/qp-suspense-demo/src/app/app.module.ts
+++ apps/qp-suspense-demo/src/app/app.module.ts
@@ -5,27 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorViewDirective, DefaultViewDirective, FallbackViewDirective } from '@queplan/qp-suspense/directives';
 import { SuspenseComponent } from '@queplan/qp-suspense';
-import { NotLazyComponent } from './not-lazy/not-lazy.component';
-import { NotLazy2Component } from './not-lazy2/not-lazy2.component';
-import { NotLazyEventdrivenComponent } from './not-lazy-eventdriven/not-lazy-eventdriven.component';
-import { TestSuspenseableComponent } from './test-suspenseable/test-suspenseable.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotLazyComponent,
-    NotLazy2Component,
-    NotLazyEventdrivenComponent,
-    TestSuspenseableComponent,
   ],
   imports: [
     BrowserModule, 
-    AppRoutingModule, 
-    HttpClientModule, 
-    SuspenseComponent,
-    DefaultViewDirective,
-    FallbackViewDirective,
-    ErrorViewDirective
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [ AppComponent ],
